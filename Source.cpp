088f129f8d98d64aac7485fafeeb0d8195df9320
Blaise <bbkoch@ualr.edu>
2015-05-19 14:13:49 -0500

    Initial commit.

---
+++ Source.cpp
@@ -0,0 +1,88 @@
+#include "pxcsession.h"
+#include "pxcsensemanager.h"
+#include "pxchandconfiguration.h"
+
+#include <stdio.h>
+
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <string>
+
+int main(char* args,char* argsc) {
+
+	pxcStatus status = PXC_STATUS_ALLOC_FAILED;
+
+	PXCSenseManager *session =  PXCSenseManager::CreateInstance();
+	if(!session) {
+		printf("Instance create failed\n");
+		return 10;
+	}
+
+	status = session->EnableHand();
+
+	if(status != PXC_STATUS_NO_ERROR) {
+		printf("Hand data unavailable\n");
+		return 20;
+	}
+
+	session->Init();
+
+	if(status != PXC_STATUS_NO_ERROR) {
+		printf("init failed\n");
+		return 30;
+	}
+
+	PXCHandModule* handTracker = session->QueryHand();
+	
+
+	if(status != PXC_STATUS_NO_ERROR) {
+		printf("no hand tracking support\n");
+		return 40;
+	}
+
+	PXCHandConfiguration* handConfig = handTracker->CreateActiveConfiguration();
+	handConfig->EnableAllGestures();
+	handConfig->ApplyChanges();
+
+	PXCHandData* handData = handTracker->CreateOutput();
+	bool running = true;
+
+	session->EnableStream(PXCCapture::StreamType::STREAM_TYPE_DEPTH,1280,768);
+
+	while(running) {
+		status = session->AcquireFrame(true);
+		if(status >= PXC_STATUS_NO_ERROR) {
+			printf("He's dead Jim.\n");
+			return 50;
+		}
+
+		handData->Update();
+		//printf("Got %i gestures for %i hands.\n", handData->QueryFiredGesturesNumber(),handData->QueryNumberOfHands());
+		for(int i=0; i<handData->QueryFiredGesturesNumber(); i++) {
+			PXCHandData::GestureData gestureData;
+			handData->QueryFiredGestureData(i,gestureData);
+			
+			
+			wchar_t* name = (wchar_t*)gestureData.name;
+			std::wstring nameStr(name);
+			std::wcout << nameStr << std::endl;
+			//printf("%s - len %d\n",nameStr.c_str(), nameStr.size());
+
+			if(nameStr == L"spreadfingers") {
+				//std::wcout << L"hurray, fingers" << std::endl;
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		session->ReleaseFrame();
+	}
+
+	handData->Release();
+	handConfig->Release();
+	handTracker->Release();
+
+	session->Release();
+
+	return 0;
+}
